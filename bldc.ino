e7f660079fb110f47b90b52ae105b08b9d76f7d4
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2023-10-07 19:35:41 +0100

    bldc, works, no sensor

---
+++ bldc.ino
@@ -0,0 +1,91 @@
+#include <SimpleFOC.h>
+
+// init BLDC motor
+BLDCMotor motor = BLDCMotor( 11 );
+// init driver
+BLDCDriver3PWM driver = BLDCDriver3PWM(11, 10, 9, 8);
+//  init encoder
+Encoder encoder = Encoder(2, 3, 2048);
+// channel A and B callbacks
+void doA(){encoder.handleA();}
+void doB(){encoder.handleB();}
+
+// commander interface
+Commander command = Commander(Serial);
+void onTarget(char* cmd){ command.motion(&motor, cmd); }
+
+
+
+void setup() {
+
+  pinMode(12,OUTPUT); // declares pin 12 as output and sets it to LOW
+
+  // initialize encoder hardware
+  encoder.init();
+  // hardware interrupt enable
+  encoder.enableInterrupts(doA, doB);
+  // link the motor to the sensor
+  motor.linkSensor(&encoder);
+
+  // power supply voltage
+  // default 12V
+  driver.voltage_power_supply = 12;
+  driver.init();
+  // link the motor to the driver
+  motor.linkDriver(&driver);
+
+  // set control loop to be used
+  motor.controller = MotionControlType::angle_openloop; // XXX: openloop
+  
+  // controller configuration based on the control type 
+  // velocity PI controller parameters
+  // default P=0.5 I = 10
+  motor.PID_velocity.P = 0.2;
+  motor.PID_velocity.I = 20;
+  
+  //default voltage_power_supply
+  motor.voltage_limit = 6;
+
+  // velocity low pass filtering
+  // default 5ms - try different values to see what is the best. 
+  // the lower the less filtered
+  motor.LPF_velocity.Tf = 0.02;
+
+  // angle P controller 
+  // default P=20
+  motor.P_angle.P = 20;
+  //  maximal velocity of the position control
+  // default 20
+  motor.velocity_limit = 4;
+  
+  // initialize motor
+  motor.init();
+  // align encoder and start FOC
+  motor.initFOC();
+
+  // add target command T
+  command.add('T', onTarget, "motion control");
+
+  // monitoring port
+  Serial.begin(115200);
+  Serial.println("Motor ready.");
+  Serial.println("Set the target angle using serial terminal:");
+  _delay(1000);
+}
+
+
+void loop() {
+  // iterative FOC function
+  motor.loopFOC();
+
+  float target = (float)millis() / 1000.0 / 60.0 * 3.14;
+
+  Serial.print(target);
+
+  // function calculating the outer position loop and setting the target position 
+  motor.move(target);
+
+  // commander interface with the user
+  //command.run();
+
+}
