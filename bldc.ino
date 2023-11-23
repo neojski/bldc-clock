359fe965a62918d619d9dfa00ce2bef292c59092
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2023-11-23 18:17:02 +0000

    wip

--- bldc.ino
+++ bldc.ino
@@ -1,31 +1,30 @@
 #include <SimpleFOC.h>
 
+// SDA & SLC to a4 & a5
+
 // init BLDC motor
 BLDCMotor motor = BLDCMotor( 11 );
 // init driver
 BLDCDriver3PWM driver = BLDCDriver3PWM(11, 10, 9, 8);
-//  init encoder
-Encoder encoder = Encoder(2, 3, 2048);
-// channel A and B callbacks
-void doA(){encoder.handleA();}
-void doB(){encoder.handleB();}
 
 // commander interface
 Commander command = Commander(Serial);
 void onTarget(char* cmd){ command.motion(&motor, cmd); }
 
-MagneticSensorI2C sensor = MagneticSensorI2C(0x36, 12, 0x0E, 4);
+MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
 
 void setup() {
+  Serial.begin(115200);
+  SimpleFOCDebug::enable(&Serial);
+ 
+  // is this set clock needed?
+  //Wire.setClock(400000);
+  //Wire.begin();
+  sensor.init();
 
   pinMode(12,OUTPUT); // declares pin 12 as output and sets it to LOW
 
-  // initialize encoder hardware
-  encoder.init();
-  // hardware interrupt enable
-  encoder.enableInterrupts(doA, doB);
-  // link the motor to the sensor
-  motor.linkSensor(&encoder);
+  motor.linkSensor(&sensor);
 
   // power supply voltage
   // default 12V
@@ -35,25 +34,28 @@ void setup() {
   motor.linkDriver(&driver);
 
   // set control loop to be used
-  motor.controller = MotionControlType::angle_openloop; // XXX: openloop
+  motor.controller = MotionControlType::velocity; // XXX: openloop
   
   // controller configuration based on the control type 
   // velocity PI controller parameters
   // default P=0.5 I = 10
-  motor.PID_velocity.P = 0.2;
-  motor.PID_velocity.I = 20;
+  motor.PID_velocity.P = 0.1;
+  motor.PID_velocity.I = 0;
+  motor.PID_velocity.D = 0;
+
+  //motor.sensor_direction = Direction::CCW;
   
   //default voltage_power_supply
-  motor.voltage_limit = 6;
+  motor.voltage_limit = 12;
 
   // velocity low pass filtering
   // default 5ms - try different values to see what is the best. 
   // the lower the less filtered
-  motor.LPF_velocity.Tf = 0.02;
+  motor.LPF_velocity.Tf = 0.01;
 
   // angle P controller 
   // default P=20
-  motor.P_angle.P = 20;
+  motor.P_angle.P = 2;
   //  maximal velocity of the position control
   // default 20
   motor.velocity_limit = 4;
@@ -66,32 +68,44 @@ void setup() {
   // add target command T
   //command.add('T', onTarget, "motion control");
 
-  sensor.init();
 
   // monitoring port
-  Serial.begin(115200);
   Serial.println("Motor ready.");
   Serial.println("Set the target angle using serial terminal:");
   _delay(1000);
 }
 
 
+int i = 0;
 void loop() {
   // iterative FOC function
   motor.loopFOC();
 
-  float target = (float)millis() / 1000.0 / 60.0 * 3.14;
+  //float target = (float)millis() / 1000.0 / 60.0 * 3.14;
+  float target = 0.;
 
-  //Serial.print(target);
 
-  // function calculating the outer position loop and setting the target position 
-  motor.move(target);
+  //sensor.update();
+  if (i % 100 == 0) {
+    Serial.print(target);
+    Serial.print("\t");
+
+    Serial.print(sensor.getFullRotations());
+    Serial.print("\t");
+  
+    Serial.print(sensor.getAngle() / 3.14 / 2 * 360.);
+    Serial.print("\n");
+
+
+    }
+  i++;
+
+
+  motor.move(1);
+  
 
   // commander interface with the user
   //command.run();
-
-  sensor.update();
-  Serial.print(sensor.getAngle());
-  Serial.print("\n");
   
+  //delayMicroseconds(1000); 
 }
